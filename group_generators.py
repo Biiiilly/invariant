a6c2c81ee208706613c4cfc3b0def16e94e19006
Biiiilly <bm221@ic.ac.uk>
2025-01-23 19:15:08 +0000

    m4r

--- group_generators.py
+++ group_generators.py
@@ -1,13 +1,40 @@
 import numpy as np
 
+
+def get_outer_ring(matrix):
+    n = len(matrix)
+    if n == 0:
+        return []
+    if n == 1:
+        return matrix[0]
+    
+    result = []
+    result.extend(matrix[0])
+    
+    for i in range(1, n - 1):
+        result.append(matrix[i][0])
+        result.append(matrix[i][-1])
+
+    result.extend(matrix[-1])
+    
+    return result
+
+
 def rotation_generator(n):
 
     R = np.zeros((n, n))
     order = {i: None for i in range(n**2)}
+    ring = np.array([[i * n + j + 1 for j in range(n)] for i in range(n)])
+    group = []
 
     if n % 2 != 0:
         pass
     else:
+        for i in range(n-1):
+            outer_ring = get_outer_ring(ring[i:n-i, i:n-i])
+            
+
+
         pass
 
     return R
